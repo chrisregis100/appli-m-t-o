201674c6d8a1d46f105000ff4a4ef1362b35cc7d
Erneregis <erneregis@gmail.com>
2024-07-06 15:16:28 +0100

    finalisation de la page d'accueil..

---
+++ script.js
@@ -0,0 +1,16 @@
+let current = 0;
+
+function carouselManage( ){
+const slide1 = document.getElementById("slide1");
+const slide2 = document.getElementById("slide2");
+const slide3 = document.getElementById("slide3");
+
+const  caroussel = document.querySelector('.caroussel');
+
+  const carousselItem = document.querySelector(".today");
+  
+  
+  console.log(carousselItem.length);
+
+}
+carouselManage();
